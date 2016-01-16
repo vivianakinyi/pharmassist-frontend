8a880180b3315bf4084c199a626fec6084957359
vivianakinyi <vivian.akinyi@savannahinformatics.com>
2016-01-16 17:26:04 +0300

    Initial commit for setting up the angularjs server using Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pharmassistApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pharmassistApp
+ */
+angular.module('pharmassistApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pharmassistApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pharmassistApp
+ */
+angular.module('pharmassistApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
