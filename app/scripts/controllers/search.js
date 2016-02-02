2a1bdf2b205238bf031bda8828703c17189c9873
vivianakinyi <vivian.akinyi@savannahinformatics.com>
2016-02-02 22:36:07 +0300

    Using combo box to select drugs

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -9,20 +9,19 @@
  */
 angular.module('pharmassistApp')
   .controller('SearchCtrl', function ($scope, geolocation, $http) {
-     // $http.get("http://localhost:8000/api/pharmacy/pharmacy/")
-     // .then(function(data){
       $http({
-        url: "http://localhost:8000/api/pharmacy/pharmacy/",
+        url: "http://localhost:8000/api/drugs/drugs/",
         method: "GET",
-        params: {query: {name:name}}
-      }).then(function(data){
-        $scope.drugs = data;
+        params: {name:name}
+      }).then(function(response){
+        $scope.drugs = response.data.results;
+        $scope.selected = { value: $scope.drugs[0] };
+
         console.log($scope.drugs);
-        console.log(data.results.data);
       });
 
         // $scope.data = data;
-        // $scope.selected = { value: $scope.data[0] };
+
 
     $scope.detectLocation = function() {
         geolocation.getLocation().then(function(data){
