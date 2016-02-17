9de24b2488ec09018b78d405807c8f8c12b906c6
vivianakinyi <vivian.akinyi@savannahinformatics.com>
2016-02-17 13:30:41 +0300

    Pharmacy detail view and edit form

--- app/scripts/controllers/pharmacy.js
+++ app/scripts/controllers/pharmacy.js
@@ -9,16 +9,14 @@ angular.module('pharmassistApp')
         geolocation.getLocation().then(function(data){
             var myPoint = "POINT(" + data.coords.latitude +
                          " " + data.coords.longitude + ")";
-        console.log(myPoint);
-        $scope.pharmacyDetails.point = myPoint;
-
-    });
+            $scope.pharmacyDetails.point = myPoint;
+        });
+    }
 
     $scope.savePharmacy = function(){
-        $scope.pharmacyDetails.no = 125477;
+        $scope.pharmacyDetails.no = 125479;
         console.log($scope.pharmacyDetails);
         apiService.post(url, $scope.pharmacyDetails).then(function (response) {
-            console.log(response);
             console.log("Pharmacy saved successfully");
             $location.path('/search/results');
         }, function(err) {
@@ -26,24 +24,36 @@ angular.module('pharmassistApp')
         });
         console.log('Geometry at save: ', $scope.pharmacyDetails.point);
         }
-    }
 })
   .controller('PharmacyDetailCtrl',
-    function ($scope, apiService, $routeParams) {
+    function ($scope, apiService, $routeParams,geolocation) {
+
+        // $scope.pharmacyDetails.no = 12547;
         var currentID = $routeParams.id
-        var url = "http://localhost:8000/api/pharmacy/pharmacy/";
+        var url = "http://localhost:8000/api/pharmacy/pharmacy/" + currentID;
         $scope.pharmacyDetails = {};
 
         apiService.get(url).then(function(response){
-            $scope.pharmacies = response.data.results.features;
+            console.log('Detail view', response);
+            $scope.pharmacies = response.data.properties;
             console.log($scope.pharmacies);
-            $scope.whichPharmacy = $routeParams.id;
 
         });
-
-
-
-
-  })
-
-
+        $scope.detectLocation = function(){
+            geolocation.getLocation().then(function(data){
+                var myPoint = "POINT(" + data.coords.latitude +
+                " " + data.coords.longitude + ")";
+                $scope.pharmacies.point = myPoint;
+            });
+        }
+        $scope.update = function(){
+            var endpoint = "http://localhost:8000/api/pharmacy/pharmacy/" ;
+            apiService.update(endpoint, currentID, $scope.pharmacyDetails)
+            .then(function(response){
+                console.log(response);
+                console.log("Pharmacy updated successfully");
+            }, function(err){
+                console.log(err);
+            });
+        }
+  });
