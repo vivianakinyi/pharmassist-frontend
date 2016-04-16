7a9030f98e81a10300f554bf7eae339d26752440
vivianakinyi <vivian.akinyi@savannahinformatics.com>
2016-04-17 00:47:32 +0300

    Different nav for different users

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,14 +2,19 @@
 
 angular.module('pharmassistApp')
   .controller('LoginCtrl', function ($scope, $state, djangoAuth, Validate) {
+    $scope.types = ['user', 'pharmacist', 'admin'];
+    $scope.selection = $scope.types[0];
+
     $scope.model = {'username':'','password':''};
-  	$scope.complete = false;
+    $scope.complete = false;
+
     $scope.login = function(formData){
       $scope.errors = [];
       Validate.form_validation(formData,$scope.errors);
       if(!formData.$invalid){
         djangoAuth.login($scope.model.username, $scope.model.password)
         .then(function(data){
+          console.log($scope.model.password)
         	// success case
         	$state.go("home");
         },function(data){
